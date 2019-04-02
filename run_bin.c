ba79be1be1c3f526a155fa194ca48d9ec1e47815
maxmidle <37108513+maxmidle@users.noreply.github.com>
2019-04-02 09:37:08 +0200

    Update run_bin.c
    exec cmd with realtive path [ok]

--- run_bin.c
+++ run_bin.c
@@ -18,7 +18,7 @@ int	run_bin(char **command, char **envorig, char **envexec)
 	int		y;
 
 	y = 0;
-	if (command[0][0] == '/' && !access(command[0], X_OK))
+	if (ft_strchr(command[0], '/') && !access(command[0], X_OK))
 		return (exec_bin(command[0], command, envexec));
 	if ((y = env_search(envorig, "PATH")) == -1)
 		return (0);
